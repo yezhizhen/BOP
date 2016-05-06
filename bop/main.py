6b989033c79eaeccce54c62d88ab5f47cff251ec
YE ZHIZHEN <yezhizhenjiakang@gmail.com>
2016-05-06 20:21:50 +0800

    MultiThreading

--- bop/main.py
+++ bop/main.py
@@ -1,20 +1,40 @@
-import query
+from query import *
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qsl
+from multiprocessing.pool import ThreadPool
 import json
 
 port = 80
-hostname = "eulervm.chinacloudapp.cn"
+
 
 class RestServer(BaseHTTPRequestHandler):
-
+    
+    def threaded_function(expr):
+        return send_query(expr)
+    
+    def process_case1(self, id1, id2, path):
+        #get information of id1,id2.
+        #id1
+        pool = ThreadPool(processes=1)
+        async_result1 = pool.apply_async(threaded_function, ("Id="+id1,))
+        
+        #id2
+        res2 = send_query("Id="+id2)
+        res1 = async_result1.get()
+        
+        #one hop
+        
+        
+        
+    
     def do_GET(self):
         url = self.path
         que = dict(parse_qsl(urlparse(url).query))
         
         #process the "que"(containing id1, id2) and get the path...
+        path = []
         
-        
+        self.process_case1(que["id1"],que["id2"],path)
         
         
         #path is the final value we return
@@ -32,7 +52,7 @@ class RestServer(BaseHTTPRequestHandler):
 '''
 url = "https://oxfordhk.azure-api.net/academic/v1.0/evaluate?expr=Id=2140251882&count=10000&attributes=Id,AA.AuId,AA.AfId&subscription-key=f7cc29509a8443c5b3a5e56b0e38b5a6"
 
-response = query.send_query(url)
+response = send_query("Id=2140251882&count=10000")
 
 print(response)
 '''
--- bop/query.py
+++ bop/query.py
@@ -1,4 +1,10 @@
 import urllib.request
 import json
-def send_query(url):
-	return json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
+
+
+pre_eva = "https://api.projectoxford.ai/academic/v1.0/evaluate?expr="
+
+post_eva = "&attributes=Id,AA.AuId,AA.AfId,RId,F.FId,C.CId,J.JId&subscription-key=f7cc29509a8443c5b3a5e56b0e38b5a6"
+
+def send_query(expr):
+	return json.loads(urllib.request.urlopen(pre_eva+url+post_eva).read().decode("utf-8"))
